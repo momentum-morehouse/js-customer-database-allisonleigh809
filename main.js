a39afa9e786714c996c5af66878f5e36a42fe91c
allisonleigh808 <allisonleigh808@gmail.com>
2020-06-16 22:33:37 +0000

    idk if i did it right but i put something anyway. i think i did step 1

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import customers from './customers.js'
+console.log(customers);
+
+// let customer1 =
+//   name: "Mr.Curtis Ryan",
+//     address: "8073 Rolling Green Rd. South Valley, Missouri 99356",
+//       dateOfBirth: "October 29, 1967",
+//         phoneNumber: "518-664-5821",
+//           dateOfRegistration:
+// "January 12, 2007",
+//   imageUrl: 'https://randomuser.me/api/portraits/thumb/men/54.jpg' 
